c3889d6fe8ace55604490e8cc67853fc30a9dd27
Arthaud <arthaudp33@gmail.com>
2024-09-10 13:50:12 +0200

    :white_check_mark: Fix homepage if no quotes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,6 +5,7 @@
             <p class="mt-2 text-2xl md:text-4xl">Des citations philosophiques et poétiques.</p>
         </section>
 
+        @if($lastQuotes->count())
         <section>
             <h2 class="text-2xl sm:text-3xl mt-40">Citation au hasard</h2>
             <div class="mt-4">
@@ -22,5 +23,6 @@
                 <x-quote.grid :quotes="$lastQuotes" />
             </div>
         </section>
+        @endif
     </div>
 </x-layouts.app>
--- tests/Feature/HomeTest.php
+++ tests/Feature/HomeTest.php
@@ -9,7 +9,14 @@ use Tests\TestCase;
 class HomeTest extends TestCase
 {
     use RefreshDatabase;
-    
+
+    public function test_works_if_no_quote(): void
+    {
+        $response = $this->get("/");
+
+        $response->assertOk();
+    }
+
     public function test_only_display_validated_quotes(): void
     {
         $validated = Quote::factory()->validated()->create();
